162d33335427b4cdb1dfc570aa194dcb8554afa2
Kandy1116 <venus.kandy@gmail.com>
2026-03-20 12:33:36 -0500

    fix: remove unused imports

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Price from "./ui/Price";
 import Ratings from "./ui/Ratings";
